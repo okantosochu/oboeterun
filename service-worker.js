1ac7e7c7643063d331f60cffc55e9d2a69b58218
おかん逃走中 <okan.tosochu@gmail.com>
2026-09-03 17:42:08 +0900

    Release v0.2.1 with YouTube link

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='oboeterun-v02';
+const CACHE='oboeterun-v021';
 const ASSETS=['./','./index.html','./styles.css','./manifest.webmanifest','./js/config.js','./js/characters.js','./js/stories.js','./js/storage.js','./js/app.js','./たこぴー.png','./くらぴー.png','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
